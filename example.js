fdf0721dd2c6a001dedadbcd1c0502ceb799ce98
fibo <casati_gianluca@yahoo.it>
2020-01-16 14:03:03 +0100

    use try catch, in case there are not enough file permissions

--- example.js
+++ example.js
@@ -1,14 +1,25 @@
 const write = require('write-file-utf8')
 
+// Nested folders will be created if they do not exist yet.
 const filePath1 = '/tmp/foo/bar.txt'
 const filePath2 = '/tmp/foo/bar/quz.txt'
+
 const content = 'Hello'
 
+// Since write-file-utf8 function will return a Promise,
+// the most comfortable way to run it is inside an async function.
 async function example () {
-  await write(filePath1, content)
+  try {
+    await write(filePath1, content)
 
-  const buffer = new Buffer.from(content)
-  await write(filePath2, buffer)
+    const buffer = new Buffer.from(content) // this is an utf-8 encoded buffer
+    await write(filePath2, buffer)
+  } catch (error) {
+    // In case you do not have permissions to create folders,
+    // you may want to handle that case here.
+    console.error(error)
+  }
 }
 
+// Run example.
 example()
